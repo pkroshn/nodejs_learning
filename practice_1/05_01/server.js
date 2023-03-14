5e714cf0b6a0063780ac8c3075bf8f90211b48d2
pkroshn <pkumararoshan@hotmail.com>
2023-03-14 14:39:06 +0530

    task 5

--- practice_1/05_01/server.js
+++ practice_1/05_01/server.js
@@ -1,8 +1,15 @@
 var express = require('express')
+var bodyParser = require('body-parser')
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
 var app = express()
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
 
 var messages = [
     {name:'Tim', message:'Hi'},
@@ -13,6 +20,12 @@ app.get('/messages', (req, res) => {
     res.send(messages)
 })
 
-var server = app.listen(3000, () => {
+app.post('/messages', (req, res) => {
+    console.log(req.body)
+    messages.push(req.body)
+    res.sendStatus(200)
+})
+
+var server = http.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
 })
